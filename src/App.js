94f8fdabe73c0c28c68532a6cabf005c46c852b4
Mohd Belal Naim <belal@Mohds-MacBook-Air.local>
2023-12-02 17:24:47 +0530

    Added login,signup and forgot password functionalities

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './screens/Home';
+import BasicDetails from './screens/signup/BasicDetails';
+import UserName from './screens/signup/UserName';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/signup/basic-details" element={<BasicDetails/>} />
+        <Route path="/signup/username" element={<UserName/>} />
+      </Routes>
+    </Router>
   );
 }
 
